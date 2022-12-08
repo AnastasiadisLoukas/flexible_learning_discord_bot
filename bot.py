38769525441e33db747515642ed8bca4a7eb48b4
Loukas_Anastasiadis <78563425+AnastasiadisLoukas@users.noreply.github.com>
2022-12-08 17:48:01 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,8 +11,7 @@ bot.load_extension("testcommands")
 bot.load_extension("agentchatcommands")
 bot.load_extension('reactionroles')
 bot.load_extension("utility")
-bot.run("ODA5MTI3OTU1MzMwNjI5NzAz.YCQlIQ.5D1q8MO7GUiQaxOtiSw-1CmFt4M")
-#ODA5MTI3OTU1MzMwNjI5NzAz.YCQlIQ.5D1q8MO7GUiQaxOtiSw-1CmFt4M
-#ODE3Mzk2NzYxOTQwOTgzOTA4.YEI6Dg.sgnE3-VMp7Q2wJdG-klB9RNVuls
+bot.run("Your bot token here ")
+
 
 
